aaf6258cbda224f56ae02645c4b8ae4e66a91b5e
Morgane Goujon <morgane@scalizer.fr>
2024-06-04 15:30:18 +0200

    [FIX] unit test: Warning mail.activity.state: selection=[('overdue', 'Overdue'), ('today', 'Today'), ('planned', 'Planned'), ('done', 'Done'), ('cancel', 'Cancelled')] overrides existing selection; use selection_add instead

--- activity_dashboard_mngmnt/models/mail_activity.py
+++ activity_dashboard_mngmnt/models/mail_activity.py
@@ -33,7 +33,7 @@ class MailActivity(models.Model):
     mostly to add dashboard functionalities"""
     _inherit = "mail.activity"
 
-    state = fields.Selection([
+    state = fields.Selection(selection_add=[
         ('overdue', 'Overdue'), ('today', 'Today'), ('planned', 'Planned'),
         ('done', 'Done'), ('cancel', 'Cancelled')], string='State',
         help='Track state of activity.',
@@ -41,9 +41,9 @@ class MailActivity(models.Model):
     active = fields.Boolean(string='Active',
                             help='Default field to archive',
                             default=True)
-    type = fields.Selection(
-        [('overdue', 'Overdue'), ('today', 'Today'), ('planned', 'Planned'),
-         ('done', 'Done'), ('cancel', 'Cancelled')],
+    type = fields.Selection(selection_add=[
+        ('overdue', 'Overdue'), ('today', 'Today'), ('planned', 'Planned'),
+        ('done', 'Done'), ('cancel', 'Cancelled')],
         string='Type', help='Choose the type of activity.')
 
     activity_tag_ids = fields.Many2many('activity.tag', string='Activity Tags',
